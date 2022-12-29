4f270739da213f393fd2d9ec5dbc23f3670d96b4
Pratyush Gupta <pg0623733@gmail.com>
2022-12-29 22:29:25 +0530

    Added flights crud with few filters

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -1,6 +1,34 @@
 const { Flights } = require('../models/index');
 
+const { Op } = require('sequelize');
+
 class FlightRepository{
+
+    #createFilter(data){
+        let filter = {};
+        if(data.arrivalAirportId){
+            filter.arrivalAirportId = data.arrivalAirportId;
+        }
+        if(data.departureAirportId){
+            filter.departureAirportId = data.departureAirportId;
+        }
+        if(data.minPrice && data.maxPrice){
+            Object.assign(filter,{
+                [Op.and]:[
+                    {price:{[Op.lte]:data.maxPrice}},
+                    {price:{[Op.gte]:data.minPrice}}
+                ]
+            })
+        }
+        if(data.minPrice){
+                Object.assign(filter,{price: {[Op.gte]:data.minPrice}});
+        }
+        if(data.maxPrice){
+                Object.assign(filter,{price: {[Op.lte]:data.maxPrice}});
+        }
+        return filter;
+    }
+
     async createFlight(data){
         try{
         const flight = await Flights.create(data);
@@ -11,6 +39,42 @@ class FlightRepository{
             throw {error};
         }
     }
+
+    async getFlight(flightId){
+        try {
+            const flight = await Flights.findByPk(flightId);
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong at repository layer");
+            throw {error};
+        }
+    }
+
+    async getAllFlights(filter){
+        try {
+            const filterObject = this.#createFilter(filter);
+            const flight = await Flights.findAll({
+                where: filterObject
+            });
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong at repository layer");
+            throw{error};
+            
+        }
+    }
 }
 
 module.exports = FlightRepository;
+
+
+/*
+{
+    where:{
+        arrivalAirportId: 2,
+        departureAirportId: 4,
+        price: {[Op.gte]:4000}}
+        ===> price must be greater than or equal to 4000
+    }
+}
+ */
